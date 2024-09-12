562959a3c7c688a34dbc236ee6f740107b6c0807
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-12 20:51:43 -0300

    inputs arrumados

--- Cadastro/Script.js
+++ Cadastro/Script.js
@@ -10,11 +10,10 @@ selectElement.addEventListener("change", function() {
 function mostrarFormulario() {
     var tipo = document.getElementById("tipoCadastro").value;
 
-    // Esconde ambos os formulários inicialmente
     document.getElementById("FormularioPaciente").style.display = "none";
     document.getElementById("FormularioCuidador").style.display = "none";
 
-    // Verifica o tipo selecionado e mostra o formulário correspondente
+
     if (tipo === "Paciente") {
       document.getElementById("FormularioPaciente").style.display = "block";
     } else if (tipo === "Cuidador") {
