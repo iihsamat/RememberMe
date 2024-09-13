4c6daef3aa19e1b9291fc1c3367993335470f3ec
Stefeson <stefesongomesdealmeidanetoo@gmail.com>
2024-09-13 20:35:40 -0300

    continuação-cadastro

---
+++ Cadastro/continuacao.js
@@ -0,0 +1,8 @@
+function togglePassword(id) {
+    var input = document.getElementById(id);
+    if (input.type === "password") {
+        input.type = "text";
+    } else {
+        input.type = "password";
+    }
+}
---
+++ Cadastro/telacadastro.js
@@ -0,0 +1,61 @@
+const selectElement = document.getElementById("tipoCadastro");
+
+selectElement.addEventListener("change", function() {
+  const firstOption = this.querySelector("option[value='']");
+  if (firstOption) {
+    firstOption.remove();
+  }
+});
+
+function mostrarFormulario() {
+    var tipo = document.getElementById("tipoCadastro").value;
+
+    document.getElementById("FormularioPaciente").style.display = "none";
+    document.getElementById("FormularioCuidador").style.display = "none";
+
+
+    if (tipo === "Paciente") {
+      document.getElementById("FormularioPaciente").style.display = "block";
+    } else if (tipo === "Cuidador") {
+      document.getElementById("FormularioCuidador").style.display = "block";
+    }
+}
+
+function habilitarCampos() {
+  var tipoCadastro = document.getElementById("tipoCadastro").value;
+
+  if (tipoCadastro === "Cuidador" || tipoCadastro === "Paciente") {
+      document.getElementById("nome").disabled = false;
+      document.getElementById("cpf").disabled = false;
+      document.getElementById("dataNascimento").disabled = false;
+      document.getElementById("email").disabled = false;
+  } else {
+      alert("Por favor, selecione um tipo de cadastro.");
+  }
+}
+
+function verificarCadastro() {
+  var tipoCadastro = document.getElementById("tipoCadastro").value;
+
+  var nome = document.getElementById("nome").value;
+  var cpf = document.getElementById("cpf").value;
+  var dataNascimento = document.getElementById("dataNascimento").value;
+  var email = document.getElementById("email").value;
+
+  if (nome === "" || cpf === "" || dataNascimento === "" || email === "") {
+    alert("Por favor, preencha todos os campos.");
+    return;
+  }
+
+  if (tipoCadastro === "") {
+      alert("Por favor, selecione um tipo de cadastro.");
+      return;
+  }
+
+
+  if (tipoCadastro === "Cuidador") {
+      window.location.href = "../Cadastro/Ccuidador.html";
+  } else if (tipoCadastro === "Paciente") {
+      window.location.href = "../Cadastro/Cpaciente.html";
+}
+}
