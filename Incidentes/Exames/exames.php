d8feea119b7529a2d8af5a4c6c03183057ca439f
dreaMykj <gabrielgreghi2018@gmail.com>
2024-10-28 01:23:44 -0300

    Adicionando PHP

--- Incidentes/Exames/exames.php
+++ Incidentes/Exames/exames.php
@@ -12,7 +12,7 @@
         die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
     }
 
-    $sql = "INSERT INTO tb_paciente (cd_pedido, dt_entrada, dt_impressao, nm_medico_solicitante, dt_nascimento, nm_laboratorio)
+    $sql = "INSERT INTO tb_exames (cd_pedido, dt_entrada, dt_impressao, nm_medico_solicitante, dt_nascimento, nm_laboratorio)
             VALUES ('$cd_pedido','$dt_entrada', '$dt_impressao', '$nm_medico_solicitante', '$dt_nascimento', '$nm_laboratorio')";
 
     if (mysqli_query($conexao, $sql)) {
